b12ff3275ad039599d49ec64e788a797880246f9
Swikar Yadav <swikaryadav1780@gmail.com>
2026-06-12 01:05:10 +0530

    Fix CORS for production frontend

--- src/main/java/com/swikar/rag/controller/TestController.java
+++ src/main/java/com/swikar/rag/controller/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 public class TestController {
 
     private final EmbeddingService embeddingService;
--- src/main/java/com/swikar/rag/controller/UploadController.java
+++ src/main/java/com/swikar/rag/controller/UploadController.java
@@ -11,7 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 public class UploadController {
     private final DocumentService documentService;
 
